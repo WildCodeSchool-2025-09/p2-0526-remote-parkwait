ba4545c2fdbc25bb3c5eae5b7585b187b9482e2f
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2026-07-02 11:14:56 +0200

    Update Vite server proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://urlofyourapiblockedbycors",
-  //       changeOrigin: true
-  //     }
-  //   }
-  // }
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://queue-times.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  }
